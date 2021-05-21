2e1ddac930b4b44ffd7a722e8e06a5986e1b03da
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 23:00:01 +0000

    add UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks-report-rescaled (benchmarkdotnet) benchmark result for 039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8

--- benchmarks/netcoreapp21/Baseline/Construction/history/report-rescaled/data.js
+++ benchmarks/netcoreapp21/Baseline/Construction/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621627948458,
+  "lastUpdate": 1621638000610,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks-report-rescaled": [
@@ -72,6 +72,42 @@ window.BENCHMARK_DATA = {
             "value": 279.2190188453311,
             "unit": "ns",
             "range": "± 6.625677875788375"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8",
+          "message": "Manual publish N1 (short)",
+          "timestamp": "2021-05-22T01:07:01+03:00",
+          "tree_id": "49ff670af70e4c4de72523822f7a08fb500e8b5e",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8"
+        },
+        "date": 1621637961032,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks.Parse(textValue: \"1234.56789\")",
+            "value": 158.70320829318865,
+            "unit": "ns",
+            "range": "± 1.4888349809443104"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks.Decimal_Parse(textValue: \"1234.56789\")",
+            "value": 262.8779063387549,
+            "unit": "ns",
+            "range": "± 0.6712599403394685"
           }
         ]
       }
