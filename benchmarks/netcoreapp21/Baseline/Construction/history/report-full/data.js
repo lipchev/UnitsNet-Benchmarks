e9e2d509d9e73c32f78d71a796e22fa901720662
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 22:58:57 +0000

    add UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks-report-full (benchmarkdotnet) benchmark result for 039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8

--- benchmarks/netcoreapp21/Baseline/Construction/history/report-full/data.js
+++ benchmarks/netcoreapp21/Baseline/Construction/history/report-full/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621627897364,
+  "lastUpdate": 1621637936645,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks-report-full": [
@@ -72,6 +72,42 @@ window.BENCHMARK_DATA = {
             "value": 284.95399951934814,
             "unit": "ns",
             "range": "± 9.382297129830635"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8",
+          "message": "Manual publish N1 (short)",
+          "timestamp": "2021-05-22T01:07:01+03:00",
+          "tree_id": "49ff670af70e4c4de72523822f7a08fb500e8b5e",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/039ade9a6c5b4c6dc5adc8b6c12e531df09ee6e8"
+        },
+        "date": 1621637895906,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks.Parse(textValue: \"1234.56789\")",
+            "value": 173.23248386383057,
+            "unit": "ns",
+            "range": "± 1.625137793911953"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Baseline.StableBaselineBenchmarks.Decimal_Parse(textValue: \"1234.56789\")",
+            "value": 286.94437344868976,
+            "unit": "ns",
+            "range": "± 0.7327137745600547"
           }
         ]
       }
