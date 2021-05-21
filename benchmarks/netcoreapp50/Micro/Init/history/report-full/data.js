f74bb1eca3b754642011bebcd1087e5e93efee89
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 20:27:58 +0000

    add UnitsNet.Benchmark.Micro.Init.InitializationBenchmarks-report-full (benchmarkdotnet) benchmark result for d92ef51c61a7e085132654a7e0f87c40a37690e6

--- benchmarks/netcoreapp50/Micro/Init/history/report-full/data.js
+++ benchmarks/netcoreapp50/Micro/Init/history/report-full/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621564918695,
+  "lastUpdate": 1621628877613,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "UnitsNet.Benchmark.Micro.Init.InitializationBenchmarks-report-full": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 539781.914893617,
             "unit": "ns",
             "range": "± 54814.916270629474"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "d92ef51c61a7e085132654a7e0f87c40a37690e6",
+          "message": "manually publish the results (-job short)",
+          "timestamp": "2021-05-21T22:29:31+03:00",
+          "tree_id": "aa0beff90e0f3392d399f915ac9bf717d8f63a5f",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/d92ef51c61a7e085132654a7e0f87c40a37690e6"
+        },
+        "date": 1621628849067,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.Micro.Init.InitializationBenchmarks.InitUnitAbbreviationsCache",
+            "value": 1958015.625,
+            "unit": "ns",
+            "range": "± 35946.86517165649"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Init.InitializationBenchmarks.InitUnitConverter",
+            "value": 596100,
+            "unit": "ns",
+            "range": "± 12297.967311714567"
           }
         ]
       }
