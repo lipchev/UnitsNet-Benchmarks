b8978e5486669f2ebaf80b454487e6165944ce9b
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 20:23:29 +0000

    add UnitsNet.Benchmark.Micro.Construction.QuantityFromString-report-rescaled (benchmarkdotnet) benchmark result for d92ef51c61a7e085132654a7e0f87c40a37690e6

--- benchmarks/netcoreapp50/Micro/Construction/QuantityFromString/history/report-rescaled/data.js
+++ benchmarks/netcoreapp50/Micro/Construction/QuantityFromString/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621564721943,
+  "lastUpdate": 1621628608872,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "UnitsNet.Benchmark.Micro.Construction.QuantityFromString-report-rescaled": [
@@ -23,6 +23,78 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/a5d6bf983c8ffe01c4f77a18a7a365c879aab22a"
         },
         "date": 1621564708929,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.QuantityTryParse_InvalidNumber",
+            "value": 52275.64621707003,
+            "unit": "ns",
+            "range": "± 1516.8107237288343"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.TryParse_Valid",
+            "value": 52511.47963964445,
+            "unit": "ns",
+            "range": "± 1490.2181236473862"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.Parse",
+            "value": 53583.64712732919,
+            "unit": "ns",
+            "range": "± 831.045528724785"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.QuantityTryParse_InvalidUnit",
+            "value": 57862.59881422924,
+            "unit": "ns",
+            "range": "± 1786.8564563608795"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.Quantity_TryParse",
+            "value": 58115.31108688629,
+            "unit": "ns",
+            "range": "± 1778.1299970457576"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.TryParse_InvalidNumber",
+            "value": 58211.29596766242,
+            "unit": "ns",
+            "range": "± 1216.880138342836"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.Quantity_Parse",
+            "value": 60597.837438677336,
+            "unit": "ns",
+            "range": "± 3399.312897883797"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Construction.QuantityFromString.TryParse_InvalidUnit",
+            "value": 62519.09145645836,
+            "unit": "ns",
+            "range": "± 936.7128340678789"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "d92ef51c61a7e085132654a7e0f87c40a37690e6",
+          "message": "manually publish the results (-job short)",
+          "timestamp": "2021-05-21T22:29:31+03:00",
+          "tree_id": "aa0beff90e0f3392d399f915ac9bf717d8f63a5f",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/d92ef51c61a7e085132654a7e0f87c40a37690e6"
+        },
+        "date": 1621628579609,
         "tool": "benchmarkdotnet",
         "benches": [
           {
