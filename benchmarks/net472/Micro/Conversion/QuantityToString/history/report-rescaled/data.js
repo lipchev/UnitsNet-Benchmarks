9e51c8f160b242a4a0c42cf3987cbb02fe3552c9
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 03:06:18 +0000

    add UnitsNet.Benchmark.Micro.Conversion.QuantityToString-report-rescaled (benchmarkdotnet) benchmark result for a5d6bf983c8ffe01c4f77a18a7a365c879aab22a

--- benchmarks/net472/Micro/Conversion/QuantityToString/history/report-rescaled/data.js
+++ benchmarks/net472/Micro/Conversion/QuantityToString/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621561587067,
+  "lastUpdate": 1621566378511,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "UnitsNet.Benchmark.Micro.Conversion.QuantityToString-report-rescaled": [
@@ -1211,6 +1211,138 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/e4524cfe92bf8996444a5be8f44879817ad34fb7"
         },
         "date": 1621561554740,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 m, format: \"a\", culture: )",
+            "value": 639.8628683651195,
+            "unit": "ns",
+            "range": "± 30.72156249098057"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 m, format: \"a\", culture: )",
+            "value": 706.7232745034354,
+            "unit": "ns",
+            "range": "± 10.351466149744084"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 m, format: \"v\", culture: )",
+            "value": 838.2554610570272,
+            "unit": "ns",
+            "range": "± 9.981147115028964"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 m, format: \"v\", culture: )",
+            "value": 839.5748166476978,
+            "unit": "ns",
+            "range": "± 16.934472295168778"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 ft, format: \"a2\", culture: )",
+            "value": 863.5917118617466,
+            "unit": "ns",
+            "range": "± 10.126805676126496"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 ft, format: \"a2\", culture: )",
+            "value": 866.029196519118,
+            "unit": "ns",
+            "range": "± 10.487135343749815"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 m, format: \"f2\", culture: )",
+            "value": 1913.022522304369,
+            "unit": "ns",
+            "range": "± 48.480173175821086"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 m, format: \"f2\", culture: )",
+            "value": 1943.6123711722237,
+            "unit": "ns",
+            "range": "± 30.145224642463788"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 ft, format: null, culture: ru-RU)",
+            "value": 2182.918472290039,
+            "unit": "ns",
+            "range": "± 48.806966890225425"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 m, format: null, culture: )",
+            "value": 2249.4009494781494,
+            "unit": "ns",
+            "range": "± 43.24862667383593"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 ft, format: null, culture: ru-RU)",
+            "value": 2255.7159597223454,
+            "unit": "ns",
+            "range": "± 51.68984970161733"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 ft, format: null, culture: )",
+            "value": 2362.7954595229203,
+            "unit": "ns",
+            "range": "± 46.953834255436426"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 8.1e-04 m, format: null, culture: )",
+            "value": 2363.4782791137695,
+            "unit": "ns",
+            "range": "± 24.09096487548939"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 1,234.56 ft, format: null, culture: )",
+            "value": 2370.9033693586075,
+            "unit": "ns",
+            "range": "± 20.41187317846046"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 1,234.56 m, format: null, culture: )",
+            "value": 2507.6314770445533,
+            "unit": "ns",
+            "range": "± 146.09282363836144"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 8.1e-04 m, format: null, culture: )",
+            "value": 2571.5317096350327,
+            "unit": "ns",
+            "range": "± 106.94435430816209"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.IQuantity_ToString(quantity: 8.1e-04 m, format: \"s4\", culture: )",
+            "value": 2593.242655667392,
+            "unit": "ns",
+            "range": "± 109.03958901283507"
+          },
+          {
+            "name": "UnitsNet.Benchmark.Micro.Conversion.QuantityToString.ToString_(quantity: 8.1e-04 m, format: \"s4\", culture: )",
+            "value": 2654.8108673095703,
+            "unit": "ns",
+            "range": "± 60.14741786581278"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "a5d6bf983c8ffe01c4f77a18a7a365c879aab22a",
+          "message": "Publish with max-parallel: 1",
+          "timestamp": "2021-05-21T05:14:32+03:00",
+          "tree_id": "64100eaefb345a9dd17d5a1fa6e6ccb124cdbe56",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/a5d6bf983c8ffe01c4f77a18a7a365c879aab22a"
+        },
+        "date": 1621566367860,
         "tool": "benchmarkdotnet",
         "benches": [
           {
