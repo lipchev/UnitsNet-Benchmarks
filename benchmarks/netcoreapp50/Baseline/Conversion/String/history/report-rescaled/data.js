8df6c662ece47d4ac93678c2ae15dbc4d8d720ba
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 21:52:43 +0000

    add Baseline.BaselineBenchmarks-report-rescaled (benchmarkdotnet) benchmark result for fb5ca97684f9b8e84bd4bfbdf83bd511eba73370

--- benchmarks/netcoreapp50/Baseline/Conversion/String/history/report-rescaled/data.js
+++ benchmarks/netcoreapp50/Baseline/Conversion/String/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621698006106,
+  "lastUpdate": 1621720363182,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "Baseline.BaselineBenchmarks-report-rescaled": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 113.83666197458903,
             "unit": "ns",
             "range": "± 2.7406077912497278"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "fb5ca97684f9b8e84bd4bfbdf83bd511eba73370",
+          "message": "Publish results (Test Run1)",
+          "timestamp": "2021-05-22T23:26:55+03:00",
+          "tree_id": "7d14fd79414a35099140e5b52af71b53051e0a4b",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/fb5ca97684f9b8e84bd4bfbdf83bd511eba73370"
+        },
+        "date": 1621720343892,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Baseline.BaselineBenchmarks.ToString_",
+            "value": 270.3275865910481,
+            "unit": "ns",
+            "range": "± 3.7486839535929857"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_ToString",
+            "value": 108.78509190335544,
+            "unit": "ns",
+            "range": "± 1.82903259571408"
           }
         ]
       }
