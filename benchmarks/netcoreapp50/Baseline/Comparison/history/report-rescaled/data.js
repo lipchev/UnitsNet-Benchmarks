c10b2188aa3519a841223b0df9211d3a52806a21
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 21:57:31 +0000

    add Baseline.BaselineBenchmarks-report-rescaled (benchmarkdotnet) benchmark result for fb5ca97684f9b8e84bd4bfbdf83bd511eba73370

--- benchmarks/netcoreapp50/Baseline/Comparison/history/report-rescaled/data.js
+++ benchmarks/netcoreapp50/Baseline/Comparison/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621698460289,
+  "lastUpdate": 1621720651117,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "Baseline.BaselineBenchmarks-report-rescaled": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 6.472459300598849,
             "unit": "ns",
             "range": "± 0.08801270300280861"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "fb5ca97684f9b8e84bd4bfbdf83bd511eba73370",
+          "message": "Publish results (Test Run1)",
+          "timestamp": "2021-05-22T23:26:55+03:00",
+          "tree_id": "7d14fd79414a35099140e5b52af71b53051e0a4b",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/fb5ca97684f9b8e84bd4bfbdf83bd511eba73370"
+        },
+        "date": 1621720606941,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Baseline.BaselineBenchmarks.Equals",
+            "value": 0.4911205628808547,
+            "unit": "ns",
+            "range": "± 0.004507555863059205"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_Equals",
+            "value": 5.4130705912555666,
+            "unit": "ns",
+            "range": "± 0.07492727311783694"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.MinMax",
+            "value": 0.4018822502241271,
+            "unit": "ns",
+            "range": "± 0.004466009894110589"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_MinMax",
+            "value": 6.70634638604079,
+            "unit": "ns",
+            "range": "± 0.09647667771386617"
           }
         ]
       }
