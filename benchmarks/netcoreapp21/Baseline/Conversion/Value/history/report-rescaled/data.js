c61eecf640bb3b8a2f78bf11562445300a418d39
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 15:12:06 +0000

    add Baseline.BaselineBenchmarks-report-rescaled (benchmarkdotnet) benchmark result for b28d03926b63bbd5c5b8c64b5e3c487189ca05f0

---
+++ benchmarks/netcoreapp21/Baseline/Conversion/Value/history/report-rescaled/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1621696325958,
+  "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
+  "entries": {
+    "Baseline.BaselineBenchmarks-report-rescaled": [
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "b28d03926b63bbd5c5b8c64b5e3c487189ca05f0",
+          "message": "Response code 404 (Not Found)",
+          "timestamp": "2021-05-22T17:15:43+03:00",
+          "tree_id": "f4c419cb0893742777cdef9e2bdf0b02587d8831",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/b28d03926b63bbd5c5b8c64b5e3c487189ca05f0"
+        },
+        "date": 1621696277000,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Baseline.BaselineBenchmarks.ToValue",
+            "value": 8.913740114642474,
+            "unit": "ns",
+            "range": "± 0.15226599758372816"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_ToValue",
+            "value": 851.527389691346,
+            "unit": "ns",
+            "range": "± 22.906465416833864"
+          }
+        ]
+      }
+    ]
+  }
+}
