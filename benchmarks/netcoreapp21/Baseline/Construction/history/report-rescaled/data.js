bb617c628e4c252b019755bd64e5897f3e7a8553
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 21:30:58 +0000

    add Baseline.BaselineBenchmarks-report-rescaled (benchmarkdotnet) benchmark result for fb5ca97684f9b8e84bd4bfbdf83bd511eba73370

--- benchmarks/netcoreapp21/Baseline/Construction/history/report-rescaled/data.js
+++ benchmarks/netcoreapp21/Baseline/Construction/history/report-rescaled/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621696478793,
+  "lastUpdate": 1621719058097,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "Baseline.BaselineBenchmarks-report-rescaled": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 241.3910476784957,
             "unit": "ns",
             "range": "± 8.152116116246214"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "fb5ca97684f9b8e84bd4bfbdf83bd511eba73370",
+          "message": "Publish results (Test Run1)",
+          "timestamp": "2021-05-22T23:26:55+03:00",
+          "tree_id": "7d14fd79414a35099140e5b52af71b53051e0a4b",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/fb5ca97684f9b8e84bd4bfbdf83bd511eba73370"
+        },
+        "date": 1621718999881,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Baseline.BaselineBenchmarks.Parse(textValue: \"1234.56789\")",
+            "value": 165.9468092621788,
+            "unit": "ns",
+            "range": "± 2.690707859562049"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_Parse(textValue: \"1234.56789\")",
+            "value": 259.1968305561871,
+            "unit": "ns",
+            "range": "± 4.449451388048835"
           }
         ]
       }
