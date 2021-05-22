dd07930f8a98c37abafd6940273862aac50d863f
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 20:56:33 +0000

    add Baseline.BaselineBenchmarks-report-full (benchmarkdotnet) benchmark result for fb5ca97684f9b8e84bd4bfbdf83bd511eba73370

--- benchmarks/net472/Baseline/Construction/history/report-full/data.js
+++ benchmarks/net472/Baseline/Construction/history/report-full/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621694605554,
+  "lastUpdate": 1621716993290,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "Baseline.BaselineBenchmarks-report-full": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 305.6712277730306,
             "unit": "ns",
             "range": "± 3.822478508872775"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "fb5ca97684f9b8e84bd4bfbdf83bd511eba73370",
+          "message": "Publish results (Test Run1)",
+          "timestamp": "2021-05-22T23:26:55+03:00",
+          "tree_id": "7d14fd79414a35099140e5b52af71b53051e0a4b",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/fb5ca97684f9b8e84bd4bfbdf83bd511eba73370"
+        },
+        "date": 1621716932878,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Baseline.BaselineBenchmarks.Parse(textValue: \"1234.56789\")",
+            "value": 180.84271387620404,
+            "unit": "ns",
+            "range": "± 4.936900273306338"
+          },
+          {
+            "name": "Baseline.BaselineBenchmarks.Decimal_Parse(textValue: \"1234.56789\")",
+            "value": 309.27892049153644,
+            "unit": "ns",
+            "range": "± 4.442473376245799"
           }
         ]
       }
