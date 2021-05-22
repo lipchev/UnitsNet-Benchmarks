a1a10f633ace27ad559d3eed8353dfd1c312aedb
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 15:36:25 +0000

    add Micro.Init.InitializationBenchmarks-report-full (benchmarkdotnet) benchmark result for b28d03926b63bbd5c5b8c64b5e3c487189ca05f0

---
+++ benchmarks/netcoreapp50/Micro/Init/history/report-full/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1621697784971,
+  "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
+  "entries": {
+    "Micro.Init.InitializationBenchmarks-report-full": [
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "b28d03926b63bbd5c5b8c64b5e3c487189ca05f0",
+          "message": "Response code 404 (Not Found)",
+          "timestamp": "2021-05-22T17:15:43+03:00",
+          "tree_id": "f4c419cb0893742777cdef9e2bdf0b02587d8831",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/b28d03926b63bbd5c5b8c64b5e3c487189ca05f0"
+        },
+        "date": 1621697733924,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Micro.Init.InitializationBenchmarks.InitUnitAbbreviationsCache",
+            "value": 1845272.012867647,
+            "unit": "ns",
+            "range": "± 36704.979712069544"
+          },
+          {
+            "name": "Micro.Init.InitializationBenchmarks.InitUnitConverter",
+            "value": 575826.0416666666,
+            "unit": "ns",
+            "range": "± 49294.18081337191"
+          }
+        ]
+      }
+    ]
+  }
+}
