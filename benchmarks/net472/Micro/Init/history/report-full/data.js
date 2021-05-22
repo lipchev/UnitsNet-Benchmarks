d4ac9137ff485659a90153ee6ee5c0b983616738
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 20:48:16 +0000

    add Micro.Init.InitializationBenchmarks-report-full (benchmarkdotnet) benchmark result for fb5ca97684f9b8e84bd4bfbdf83bd511eba73370

--- benchmarks/net472/Micro/Init/history/report-full/data.js
+++ benchmarks/net472/Micro/Init/history/report-full/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621694145231,
+  "lastUpdate": 1621716496531,
   "repoUrl": "https://github.com/lipchev/UnitsNet-Benchmarks",
   "entries": {
     "Micro.Init.InitializationBenchmarks-report-full": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 685714.7368421053,
             "unit": "ns",
             "range": "± 57336.546527927196"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "fb5ca97684f9b8e84bd4bfbdf83bd511eba73370",
+          "message": "Publish results (Test Run1)",
+          "timestamp": "2021-05-22T23:26:55+03:00",
+          "tree_id": "7d14fd79414a35099140e5b52af71b53051e0a4b",
+          "url": "https://github.com/lipchev/UnitsNet-Benchmarks/commit/fb5ca97684f9b8e84bd4bfbdf83bd511eba73370"
+        },
+        "date": 1621716437984,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Micro.Init.InitializationBenchmarks.InitUnitAbbreviationsCache",
+            "value": 3249952.001953125,
+            "unit": "ns",
+            "range": "± 63688.49160139402"
+          },
+          {
+            "name": "Micro.Init.InitializationBenchmarks.InitUnitConverter",
+            "value": 663627.2727272727,
+            "unit": "ns",
+            "range": "± 15690.585039266663"
           }
         ]
       }
